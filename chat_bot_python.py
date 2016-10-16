ba19b8ee357901a5f7dee21db5e5a9facb918ec9
Kamran Janjua <kamranejaz98@gmail.com>
2016-10-16 16:52:08 +0500

    Create chat_bot_python.py

---
+++ chat_bot_python.py
@@ -0,0 +1,14 @@
+import os
+
+
+while True:
+	i = raw_input("Enter: ")
+	if i == "hi":
+  		print "hi how are you?"  
+
+  	if i == "Good Morning":
+  		print "Good Morning to you as well."
+  
+	else:
+  		print "any way, nice to meet you"
+  
